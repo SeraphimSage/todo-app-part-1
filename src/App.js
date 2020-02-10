8df28b66da2083633d71fc19e4b509df7a0171fd
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-02-10 16:38:19 -0700

    Completes first 2 items on rubric adding items and toggling complete.

--- src/App.js
+++ src/App.js
@@ -3,62 +3,100 @@ import "./index.css";
 import todosList from "./todos.json";
 
 class App extends Component {
-  state = {
-    todos: todosList
-  };
-  render() {
-    return (
-      <section className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <input
-            className="new-todo"
-            placeholder="What needs to be done?"
-            autofocus
-          />
-        </header>
-        <TodoList todos={this.state.todos} />
-        <footer className="footer">
-          <span className="todo-count">
-            <strong>0</strong> item(s) left
-          </span>
-          <button className="clear-completed">Clear completed</button>
-        </footer>
-      </section>
-    );
-  }
+	state = {
+		todos: todosList
+	};
+
+	handleAddToDo = event => {
+		if (event.key === "Enter") {
+			const newTodo = {
+				userId: 1,
+				id: Math.floor(Math.random() * 1000000),
+				title: event.target.value,
+				completed: false
+			};
+			const newTodos = this.state.todos.slice();
+			newTodos.push(newTodo);
+			this.setState({ todos: newTodos });
+			event.target.value = "";
+		}
+	};
+
+	handleToggleComplete = (event, todoIdToToggle) => {
+		const newTodos = this.state.todos.slice();
+		const newNewTodos = newTodos.map(todo => {
+			if (todo.id === todoIdToToggle) {
+				todo.completed = !todo.completed;
+			}
+			return todo;
+		});
+		this.setState({ todo: newNewTodos });
+	};
+	render() {
+		return (
+			<section className="todoapp">
+				<header className="header">
+					<h1>todos</h1>
+					<input
+						className="new-todo"
+						placeholder="What needs to be done?"
+						onKeyDown={this.handleAddToDo}
+						autofocus
+					/>
+				</header>
+				<TodoList
+					todos={this.state.todos}
+					handleToggleComplete={this.handleToggleComplete}
+				/>
+				<footer className="footer">
+					<span className="todo-count">
+						<strong>0</strong> item(s) left
+					</span>
+					<button className="clear-completed">Clear completed</button>
+				</footer>
+			</section>
+		);
+	}
 }
 
 class TodoItem extends Component {
-  render() {
-    return (
-      <li className={this.props.completed ? "completed" : ""}>
-        <div className="view">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={this.props.completed}
-          />
-          <label>{this.props.title}</label>
-          <button className="destroy" />
-        </div>
-      </li>
-    );
-  }
+	render() {
+		return (
+			<li className={this.props.completed ? "completed" : ""}>
+				<div className="view">
+					<input
+						className="toggle"
+						type="checkbox"
+						checked={this.props.completed}
+						onChange={event =>
+							this.props.handleToggleComplete(event, this.props.id)
+						}
+					/>
+					<label>{this.props.title}</label>
+					<button className="destroy" />
+				</div>
+			</li>
+		);
+	}
 }
 
 class TodoList extends Component {
-  render() {
-    return (
-      <section className="main">
-        <ul className="todo-list">
-          {this.props.todos.map(todo => (
-            <TodoItem title={todo.title} completed={todo.completed} />
-          ))}
-        </ul>
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section className="main">
+				<ul className="todo-list">
+					{this.props.todos.map(todo => (
+						<TodoItem
+							title={todo.title}
+							completed={todo.completed}
+							id={todo.id}
+							handleToggleComplete={this.props.handleToggleComplete}
+						/>
+					))}
+				</ul>
+			</section>
+		);
+	}
 }
 
 export default App;
